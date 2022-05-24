7fc127d74e76716dcfc90c397fced956435cc3dc
Wootenblatz <wootenblatz@gmail.com>
2022-05-23 22:56:54 -0500

    Fixed error with function name for running sql

--- DekkCore/ServerCommands.ps1
+++ DekkCore/ServerCommands.ps1
@@ -115,8 +115,8 @@ function runSqlInDb() {
 function upgradeHeirlooms() {
     $updateFiveTier = "update battlenet_account_heirlooms set flags = 31 where flags != 31 and itemId not in ($threeUpgradeHeirlooms)"
     $updateThreeTier = "update battlenet_account_heirlooms set flags = 6 where flags != 6 and itemId in ($threeUpgradeHeirlooms)"
-    runSql $updateFiveTier
-    runSql $updateThreeTier
+    runSqlInDb $updateFiveTier
+    runSqlInDb $updateThreeTier
 
 }
 
